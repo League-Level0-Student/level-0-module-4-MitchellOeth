22816ddf22b9027b229c6ced1fd9d19327ddf7dd
league <league@wts13.attlocal.net>
2018-02-18 11:27:49 -0800

    ,   happyPet

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -20,10 +20,13 @@ public class FruitQuiz extends KeyAdapter {
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
-		// 1. Print out the key code variable
-
-		// 2. Make 3 int variables that hold the key codes for A, b, and C
 		
+		// 1. Print out the key code variable
+System.out.println(keyCode);
+		// 2. Make 3 int variables that hold the key codes for A, b, and C
+		int A = arg0.getKeyCode();
+		int B = arg0.getKeyCode();
+		int C = arg0.getKeyCode();
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
--- src/HappyPet.java
+++ src/HappyPet.java
@@ -4,18 +4,38 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String pet = JOptionPane.showInputDialog(null,"What pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+int happy = 0;
+ for (int i = 0; i < 5; i++) {
+	
 
+int task = JOptionPane.showOptionDialog(null, "What do you want to do to your pet to make them happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Feed it", "Water it like a plant", "force fart it" }, null);
+if (task==0) {
+	FeedPet(pet);
+	happy+=1;
+}
+if (task==1) {
+	WaterPet(pet);
+	happy+=2;
+}
+if (task==2) {
+	FartPet(pet);
+happy+=10;
+}
+if (happy>=15) {
+	JOptionPane.showMessageDialog(null, "Your pet loves you!");
+	System.exit(0);
+}
+ }
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
@@ -26,4 +46,20 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-}
+
+	public static void FeedPet(String pet) {
+		
+		JOptionPane.showMessageDialog(null, "Your "+ pet +" eats the food");
+	}
+
+	public static void WaterPet(String pet) {
+		
+		JOptionPane.showMessageDialog(null, "Your "+ pet +" got watered");
+	}
+
+	public static void FartPet(String pet) {
+		
+		JOptionPane.showMessageDialog(null, "Your "+ pet +" farted");
+	}
+	
+	}
